655993982a8304342ea3d409e236fada78f55f94
sandy85766 <71687733+sandy85766@users.noreply.github.com>
2021-01-04 22:22:17 +0800

    0104_敵人AI

---
+++ 3D_End_107051198/Assets/Scrips/Enemy.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 50)]
+    public float speed = 3;
+    [Header("停止距離"), Range(0, 50)]
+    public float stopDistance = 2.5f;
+    [Header("攻擊冷卻時間"), Range(0, 50)]
+    public float cd = 2f;
+
+    private Transform player;
+    private NavMeshAgent nav;
+    private Animator ani;
+    private float timer;  
+
+    private void Awake()
+    {
+        
+        nav = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+        
+        player = GameObject.Find("格瑞").transform;
+        
+        nav.speed = speed;
+        nav.stoppingDistance = stopDistance;
+    }
+
+    private void Update()
+    {
+        Track();
+        Attack();
+    }
+
+    // 攻擊
+    private void Attack()
+    {
+        if (nav.remainingDistance < stopDistance)
+        {
+            
+            timer += Time.deltaTime;
+
+            
+            Vector3 pos = player.position;
+           
+            pos.y = transform.position.y;
+            
+            transform.LookAt(pos);
+
+            if (timer >= cd)
+            {
+                ani.SetTrigger("攻擊觸發");
+                timer = 0;
+            }
+
+        }
+    }
+
+    // 追蹤
+    private void Track()
+    {
+        
+        nav.SetDestination(player.position);
+        ani.SetBool("跑步開關", nav.remainingDistance > stopDistance);
+    }
+}
