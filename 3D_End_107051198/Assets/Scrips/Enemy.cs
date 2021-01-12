ef3be2482b6ca8b8d0228e2bf2f8ac333c069d34
sandy85766 <71687733+sandy85766@users.noreply.github.com>
2021-01-13 00:06:38 +0800

    0112_完成

--- 3D_End_107051198/Assets/Scrips/Enemy.cs
+++ 3D_End_107051198/Assets/Scrips/Enemy.cs
@@ -13,20 +13,23 @@ public class Enemy : MonoBehaviour
     public Transform atkPoint;
     [Header("攻擊長度"), Range(0f, 5f)]
     public float atkLength;
+    [Header("攻擊力"), Range(0, 500)]
+    public float atk = 30;
 
     private Transform player;
     private NavMeshAgent nav;
     private Animator ani;
-    private float timer;  
+    private float timer;
+    
 
     private void Awake()
     {
-        
+
         nav = GetComponent<NavMeshAgent>();
         ani = GetComponent<Animator>();
-        
+
         player = GameObject.Find("格瑞").transform;
-        
+
         nav.speed = speed;
         nav.stoppingDistance = stopDistance;
     }
@@ -38,27 +41,27 @@ public class Enemy : MonoBehaviour
     }
     private void OnDrawGizmos()
     {
-       
+
         Gizmos.color = Color.blue;
         Gizmos.DrawRay(atkPoint.position, atkPoint.forward * atkLength);
     }
 
     private RaycastHit hit;
-
+    public float hp = 100;
 
     // 攻擊
     private void Attack()
     {
         if (nav.remainingDistance < stopDistance)
         {
-            
+
             timer += Time.deltaTime;
 
-            
+
             Vector3 pos = player.position;
-           
+
             pos.y = transform.position.y;
-            
+
             transform.LookAt(pos);
 
             if (timer >= cd)
@@ -68,18 +71,37 @@ public class Enemy : MonoBehaviour
 
                 if (Physics.Raycast(atkPoint.position, atkPoint.forward, out hit, atkLength, 1 << 8))
                 {
-                    hit.collider.GetComponent<Player>().Damage();
+                    hit.collider.GetComponent<Player>().Damage(atk);
                 }
             }
 
         }
     }
 
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        ani.SetTrigger("受傷觸發");
+
+        if (hp <= 0)
+        {
+            Dead();
+        }
+    }
+
+    private void Dead()
+    {
+        nav.isStopped = true;   //關閉 導覽器
+        enabled = false;        //關閉腳本
+        ani.SetBool("死亡開關", true);  //死亡動畫
+    }
     // 追蹤
     private void Track()
     {
-        
+
         nav.SetDestination(player.position);
         ani.SetBool("跑步開關", nav.remainingDistance > stopDistance);
     }
+
+    
 }
--- 3D_End_107051198/Assets/Scrips/Player.cs
+++ 3D_End_107051198/Assets/Scrips/Player.cs
@@ -3,16 +3,67 @@ using Invector.vCharacterController;
 
 public class Player : MonoBehaviour
 {
-    private float hp = 100;
+    private float hp = 300;
     private Animator ani;
+    private int atkCount;
+    
+
+    private float timer;
+    [Header("連擊間隔時間"), Range(0, 3)]
+    public float interval = 1;
+    [Header("攻擊中心點")]
+    public Transform atkPoint;
+    [Header("攻擊長度"), Range(0f, 5f)]
+    public float atkLength;
+    [Header("攻擊力"), Range(0, 500)]
+    public float atk = 30;
 
     private void Awake()
     {
         ani = GetComponent<Animator>();
     }
-    public void Damage()
+
+    private void Update()
     {
-        hp -= 35;
+        Attack();
+    }
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(atkPoint.position, atkPoint.forward * atkLength);
+    }
+
+    private RaycastHit hit;
+    private void Attack()
+    {
+        if (atkCount < 4)
+        {
+            if (timer < interval)
+            {
+                timer += Time.deltaTime;
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    atkCount++;
+                    timer = 0;
+                    if (Physics.Raycast(atkPoint.position, atkPoint.forward, out hit, atkLength, 1 << 9))
+                    {
+                        hit.collider.GetComponent<Enemy>().Damage(atk);
+                    }
+                }
+            }
+            else
+            {
+                atkCount = 0;
+                timer = 0;
+            }
+        }
+        if (atkCount == 4) atkCount = 0;
+        ani.SetInteger("連擊", atkCount);
+    }
+    public void Damage(float damage)
+    {
+        hp -= damage;
         ani.SetTrigger("受傷觸發");
 
         if (hp <= 0)
